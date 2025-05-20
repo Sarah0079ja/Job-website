d1f5d9b40b1376c6c9644b5dbe2f34d604ca81d3
craftmerce <hello@craftmerce.co>
2025-05-20 16:15:31 +0200

    new1

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000,
+  base: "/Job-website/",
+  server: {
+    port: 3000,
     proxy: {
-      '/api' : {
-        target: 'http://localhost:8000',
+      "/api": {
+        target: "http://localhost:8000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
-})
+});
   
